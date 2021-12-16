1395f264c65369f11093c132b888e0cc2c5d91df
yinghueichu <yinghuei.chu@essec.edu>
2021-12-16 20:31:16 +0100

    add style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "555555555", category: "italian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "555555555", category: "italian" }
+big_mama = { name: "Big Mama", address: "Station F", phone_number: "555555555", category: "italian" }
+
+[ dishoom, pizza_east, big_mama ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
